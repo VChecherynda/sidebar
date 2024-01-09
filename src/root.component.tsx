57ff2a65947c73ef573579b9f6cc118bdecf552d
vchecherynda <vchecherynda@shutterstock.com>
2024-01-09 02:20:04 +0200

    Add data and port

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+const api = require("@home/api");
+const { List, Button, Typography } = require("@home/components");
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await api.fetchProducts();
+
+      setProducts(data?.products);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <section id="sidebar">
+      <List
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={products}
+        renderItem={(item) => (
+          <List.Item>
+            <Typography.Text>{item.brand}</Typography.Text>
+          </List.Item>
+        )}
+      />
+    </section>
+  );
 }
